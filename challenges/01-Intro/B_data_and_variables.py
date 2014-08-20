dc997a271ab09dfe2b7d4aec45c483afa218d370
BCE provisioner <bce@lists.berkeley.edu>
2014-08-20 14:18:27 -0700

    Day 2 String Commands

--- challenges/01-Intro/B_data_and_variables.py
+++ challenges/01-Intro/B_data_and_variables.py
@@ -3,20 +3,20 @@
 # instructors
 
 ## what are our names?
-instructors = ''
+instructors = 'Dav' 'Sean'
 
 # types
 ## Now define some other types of variables
 
-## some_int should be an integer
+some_int = 9
 
-## some_float should be a float
+some_float =9.0
 
 ## some_bool should be... what do you think?
 
 # redefining
 ## Here, I define a variable
-my_lucky_number = 7
+my_lucky_number = 9
 
 ## But Dav's lucky number is 9 (and he wrote the test)
 ## Redefine the value "correctly" below
